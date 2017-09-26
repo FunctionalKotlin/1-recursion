3e787eeaac172491d49b70e4ccfb08c0cb168359
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 22:47:34 +0200

    This is starting to look like Lisp

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -4,3 +4,13 @@ sealed class List<T> {
     class Empty<T> : List<T>()
     class NonEmpty<T>(val head: T, val tail: List<T>) : List<T>()
 }
+
+fun main(args: Array<String>) {
+    val cart = List.NonEmpty(
+        4, List.NonEmpty(
+            5, List.NonEmpty(
+                7, List.NonEmpty(
+                    83, List.NonEmpty(
+                        3, List.NonEmpty(
+                            4, List.Empty()))))))
+}
